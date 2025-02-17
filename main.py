758eacc8d37880d81f2c3e7f7dbea0c17639d884
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-17 21:24:11 +0530

    added a new functionality

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ breakfast_df = df[df['Meal_Type'] == 'Breakfast']
 lunch_df = df[df['Meal_Type'] == 'Lunch']
 dinner_df = df[df['Meal_Type'] == 'Dinner']
 
-def diet_Normal(total_cal,weight):
+def diet_Normal_NonVeg(total_cal,weight):
    protein = 2 * weight
    breakfast = total_cal * 0.173
    lunch = total_cal * 0.466
@@ -43,7 +43,34 @@ def diet_Normal(total_cal,weight):
    print("----------------------------------------------------------------------")
    print("Thanks for using Diet_Forge!")
 
-def diet_Bulk(total_cal,weight):
+def diet_Normal_Veg(total_cal,weight):
+   protein = 2 * weight
+   breakfast = total_cal * 0.173
+   lunch = total_cal * 0.466
+   dinner = total_cal * 0.24
+   breakfast_protein_cal = breakfast * 0.30
+   breakfast_carb_cal = breakfast * 0.50
+   breakfast_carb = breakfast_carb_cal % 4
+   lunch_protein_cal = lunch * 0.30
+   lunch_carb_cal = lunch * 0.50
+   lunch_carb = lunch_carb_cal % 4
+   dinner_protein_cal = dinner * 0.30
+   dinner_carb_cal = dinner * 0.50
+   dinner_carb = dinner_carb_cal % 4
+   breakfast_food_df = breakfast_df.loc[(df['Protein (g)'] <= protein) & (df['Calories (kcal)'] < breakfast_protein_cal) & (df['Fiber (g)'] <= breakfast_carb) & (df['Calories (kcal)'] < breakfast_carb_cal) & (df['Category'] != 'Meat')]
+   lunch_food_df = lunch_df.loc[(df['Protein (g)'] <= protein) & (df['Calories (kcal)'] < lunch_protein_cal) & (df['Fiber (g)'] <= lunch_carb) & (df['Calories (kcal)'] < lunch_carb_cal) & (df['Category'] != 'Meat')]
+   dinner_food_df = dinner_df.loc[(df['Protein (g)'] <= protein) & (df['Calories (kcal)'] < dinner_protein_cal) & (df['Fiber (g)'] <= dinner_carb) & (df['Calories (kcal)'] < dinner_carb_cal) & (df['Category'] != 'Meat')]
+
+   print("----------------------------------------------------------------------")
+   print(breakfast_food_df)
+   print("----------------------------------------------------------------------") 
+   print(lunch_food_df)
+   print("----------------------------------------------------------------------")
+   print(dinner_food_df)
+   print("----------------------------------------------------------------------")
+   print("Thanks for using Diet_Forge!")
+
+def diet_Bulk_NonVeg(total_cal,weight):
    protein = 2 * weight
    breakfast = total_cal * 0.173
    lunch = total_cal * 0.466
@@ -70,7 +97,34 @@ def diet_Bulk(total_cal,weight):
    print("----------------------------------------------------------------------")
    print("Thanks for using Diet_Forge!")
 
-def diet_Cut(total_cal,weight):
+def diet_Bulk_Veg(total_cal,weight):
+   protein = 2 * weight
+   breakfast = total_cal * 0.173
+   lunch = total_cal * 0.466
+   dinner = total_cal * 0.24
+   breakfast_protein_cal = breakfast * 0.30
+   breakfast_carb_cal = breakfast * 0.50
+   breakfast_carb = breakfast_carb_cal % 4
+   lunch_protein_cal = lunch * 0.30
+   lunch_carb_cal = lunch * 0.50
+   lunch_carb = lunch_carb_cal % 4
+   dinner_protein_cal = dinner * 0.30
+   dinner_carb_cal = dinner * 0.50
+   dinner_carb = dinner_carb_cal % 4
+   breakfast_food_df = breakfast_df.loc[(df['Protein (g)'] <= protein) & (df['Calories (kcal)'] < breakfast_protein_cal) & (df['Fiber (g)'] <= breakfast_carb) & (df['Calories (kcal)'] < breakfast_carb_cal) & (df['Category'] != 'Meat')]
+   lunch_food_df = lunch_df.loc[(df['Protein (g)'] <= protein) & (df['Calories (kcal)'] < lunch_protein_cal) & (df['Fiber (g)'] <= lunch_carb) & (df['Calories (kcal)'] < lunch_carb_cal) & (df['Category'] != 'Meat')]
+   dinner_food_df = dinner_df.loc[(df['Protein (g)'] <= protein) & (df['Calories (kcal)'] < dinner_protein_cal) & (df['Fiber (g)'] <= dinner_carb) & (df['Calories (kcal)'] < dinner_carb_cal) & (df['Category'] != 'Meat')]
+
+   print("----------------------------------------------------------------------")
+   print(breakfast_food_df)
+   print("----------------------------------------------------------------------") 
+   print(lunch_food_df)
+   print("----------------------------------------------------------------------")
+   print(dinner_food_df)
+   print("----------------------------------------------------------------------")
+   print("Thanks for using Diet_Forge!")
+
+def diet_Cut_NonVeg(total_cal,weight):
    protein = 2 * weight
    breakfast = total_cal * 0.173
    lunch = total_cal * 0.466
@@ -97,12 +151,39 @@ def diet_Cut(total_cal,weight):
    print("----------------------------------------------------------------------")
    print("Thanks for using Diet_Forge!")
 
-def calcount(BMR, active, weight):
+def diet_Cut_Veg(total_cal,weight):
+   protein = 2 * weight
+   breakfast = total_cal * 0.173
+   lunch = total_cal * 0.466
+   dinner = total_cal * 0.24
+   breakfast_protein_cal = breakfast * 0.30
+   breakfast_carb_cal = breakfast * 0.50
+   breakfast_carb = breakfast_carb_cal % 4
+   lunch_protein_cal = lunch * 0.30
+   lunch_carb_cal = lunch * 0.50
+   lunch_carb = lunch_carb_cal % 4
+   dinner_protein_cal = dinner * 0.30
+   dinner_carb_cal = dinner * 0.50
+   dinner_carb = dinner_carb_cal % 4
+   breakfast_food_df = breakfast_df.loc[(df['Protein (g)'] <= protein) & (df['Calories (kcal)'] < breakfast_protein_cal) & (df['Fiber (g)'] <= breakfast_carb) & (df['Calories (kcal)'] < breakfast_carb_cal) & (df['Category'] != 'Meat')]
+   lunch_food_df = lunch_df.loc[(df['Protein (g)'] <= protein) & (df['Calories (kcal)'] < lunch_protein_cal) & (df['Fiber (g)'] <= lunch_carb) & (df['Calories (kcal)'] < lunch_carb_cal) & (df['Category'] != 'Meat')]
+   dinner_food_df = dinner_df.loc[(df['Protein (g)'] <= protein) & (df['Calories (kcal)'] < dinner_protein_cal) & (df['Fiber (g)'] <= dinner_carb) & (df['Calories (kcal)'] < dinner_carb_cal) & (df['Category'] != 'Meat')]
+
+   print("----------------------------------------------------------------------")
+   print(breakfast_food_df)
+   print("----------------------------------------------------------------------") 
+   print(lunch_food_df)
+   print("----------------------------------------------------------------------")
+   print(dinner_food_df)
+   print("----------------------------------------------------------------------")
+   print("Thanks for using Diet_Forge!")
+
+def calcount(BMR, active, weight, veg):
    total_cal = 0
    bulk_cal = 0
    cut_cal = 0
    if(active == "Sedentary" or active == "Sed"):
-      total_cal = BMR * 1.2
+      total_cal = BMR * 1.20
    elif(active == "Lightly" or active == "L"):
       total_cal = BMR * 1.375
    elif(active == "Moderately" or active == "M"):
@@ -122,22 +203,40 @@ def calcount(BMR, active, weight):
 
    choice = input("You want a normal diet or bulking diet or cutting diet? ").capitalize()
    if(choice == 'Normal' or choice == 'N'):
-      diet_Normal(total_cal,weight)
+      if(veg == 'Y'):
+         diet_Normal_Veg(total_cal,weight)
+      elif(veg == 'N'):
+        diet_Normal_NonVeg(total_cal,weight)
+      else:
+         print("Enter correctly")
    elif(choice == 'Bulk' or choice == 'B'):
-      diet_Bulk(bulk_cal,weight)
+      if(veg == 'Y'):
+         diet_Bulk_Veg(bulk_cal,weight)
+      elif(veg == 'N'):
+        diet_Bulk_NonVeg(bulk_cal,weight)
+      else:
+         print("Enter correctly") 
    elif(choice == 'Cut' or choice == 'C'):
-      diet_Cut(total_cal,weight)
+      if(veg == 'Y'):
+         diet_Cut_Veg(cut_cal,weight)
+      elif(veg == 'N'):
+        diet_Cut_NonVeg(cut_cal,weight)
+      else:
+         print("Enter correctly") 
    else:
       print("Choose correctly")
 
-if(veg == 'Y')
+def checker(height,weight,age,gender,veg,active):
+   if(gender == 'M'):
+      BMR = (10 * weight) + (6.25 * height) - (5 * age )+ 5
+   elif(gender == 'F'):
+      BMR = (10 * weight) +( 6.25 * height) - (5 * age )- 161
+   else:
+      print('Enter correctly ')
 
-if(gender == 'M'):
-    BMR = 10 * weight + 6.25 * height - 5 * age + 5
-else:
-    BMR = 10 * weight + 6.25 * height - 5 * age - 161
+   if(age<15):
+      print("Too young for a diet")
+   else:
+      calcount(BMR,active,weight,veg)
 
-if(age<15):
-  print("Too young for a diet")
-else:
-  calcount(BMR,active,weight)
+checker(height,weight,age,gender,veg,active)
